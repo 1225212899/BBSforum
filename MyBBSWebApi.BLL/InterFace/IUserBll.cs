862d3495fa3b14445e0350560544f4443db5e9eb
1225212899 <103628933+1225212899@users.noreply.github.com>
2022-11-30 17:58:47 +0800

    Add files via upload

---
+++ MyBBSWebApi.BLL/InterFace/IUserBll.cs
@@ -0,0 +1,19 @@
+﻿using MyBBSWebApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBBSWebApi.BLL.InterFace
+{
+    public interface IUserBll
+    {
+        public List<User> GetAll();
+
+        public User CheakLogin(string userNo, string passWord);
+
+        public void Insert(int userNo, string userName, string userSex);
+
+        public void Updata(int id, int? userNo, string userName, int? userlevel, string password, string userSex);
+
+    }
+}
---
+++ MyBBSWebApi.BLL/UserBll.cs
@@ -0,0 +1,48 @@
+﻿using MyBBSWebApi.BLL.InterFace;
+using MyBBSWebApi.DAL;
+using MyBBSWebApi.Model;
+using System;
+using System.Collections.Generic;
+
+namespace MyBBSWebApi.BLL
+{
+    public class UserBll : IUserBll
+    {
+
+        public List<User> GetAll()
+        {
+            UserDal userDal = new UserDal();
+            List<User> userList = userDal.GetAll();
+            return userList.FindAll(m => m.IsDelete == false);
+        }
+
+        public User CheakLogin(string userNo, string passWord)
+        {
+            UserDal userDal = new UserDal();
+            List<User> userList = userDal.UserListGetByUserNoAndPwd(userNo, passWord);
+            if (userList.Count <= 0)
+            {
+                return default;
+            }
+            else
+            {
+                User user = userList.Find(m => m.IsDelete == false);
+                return user;
+            }
+        }
+
+        public void Insert(int userNo, string userName, string userSex)
+        {
+            UserDal userDal = new UserDal();
+            userDal.UserInsert(userNo, userName, userSex);
+        }
+
+        public void Updata(int id, int? userNo, string userName, int? userlevel, string password, string userSex)
+        {
+            UserDal userDal = new UserDal();
+            userDal.UserUpdata(id, userNo, userName, userlevel, password, userSex);
+        }
+
+       
+    }
+}
